48f613d718313f418a1b98469035ec89438fefcc
wolfwdavid <49036883+karur0su@users.noreply.github.com>
2024-10-15 17:57:21 -0400

    jwtAuth.js

---
+++ middleware/jwtAuth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'your_jwt_secret_key';
+
+module.exports = function (req, res, next) {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    if (!token) return res.status(401).json({ error: 'No token, authorization denied' });
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.user = decoded.userId;
+        next();
+    } catch (err) {
+        res.status(401).json({ error: 'Token is not valid' });
+    }
+};
